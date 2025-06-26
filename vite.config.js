179168333372886b893612dfbe4113455d66650f
Nahuel Mendarte <nahuelleandro@live.com.ar>
2025-06-25 21:54:24 -0300

    commit 21:54

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,39 @@
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: {},
-  }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
+
+
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     global: {},
+//   }
+// })
