9bd0583af7c2b965697b4993104f5ea684c00465
Nahuel Mendarte <nahuelleandro@live.com.ar>
2025-06-28 02:35:20 -0300

    primer intento de corregir el codigo en el space de github, local anda

--- src/components/RereenderisadoPadreHijo_Hijo.jsx
+++
@@ -1,8 +0,0 @@
-
-
-function RereenderisadoPadreHijo_Hijo({ texto }) {
-    console.log("El componente Hijo se renderizó");
-    return <p>{texto}</p>;
-}
-
-export default RereenderisadoPadreHijo_Hijo;
